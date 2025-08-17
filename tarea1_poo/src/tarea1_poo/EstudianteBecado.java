62fbca6def35b781dca96511c49c4851122e9264
RIVERA MEJIA RODRIGO JOSE <1720972023@mail.utec.edu.sv>
2025-08-17 08:15:11 -0600

    Agregando la base del proyecto

---
+++ tarea1_poo/src/tarea1_poo/EstudianteBecado.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea1_poo;
+
+/**
+ *
+ * @author 1720972023
+ */
+public class EstudianteBecado {
+    
+}
