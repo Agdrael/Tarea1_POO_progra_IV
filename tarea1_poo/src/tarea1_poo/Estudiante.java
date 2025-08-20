5d15c5c8de8301b89cb70318b2a5c42cbf9fc853
Daniel Obando <obandovasquezdanielisaac@gmail.com>
2025-08-20 06:50:14 -0600

    Agregados los atributos privados de la clase estudiante, constructor personalizado y metodos accesores

--- tarea1_poo/src/tarea1_poo/Estudiante.java
+++ tarea1_poo/src/tarea1_poo/Estudiante.java
@@ -9,5 +9,40 @@ package tarea1_poo;
  * @author 1720972023
  */
 public class Estudiante {
+
+    private String nombre;
+    private String carrera;
+    private String id; // se usara el tipo de identificador de la universidad? xx-xxxx-xxxx? //
     
+  
+    public Estudiante(String id, String nombre, String carrera) {
+        this.id = id;
+        this.nombre = nombre;
+        this.carrera = carrera;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
 }
