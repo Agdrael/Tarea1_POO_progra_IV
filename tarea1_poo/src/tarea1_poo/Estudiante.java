88f3e86a652fee9098a736e8b2e09c7d1d5dd36e
Daniel Obando <obandovasquezdanielisaac@gmail.com>
2025-08-21 12:21:25 -0600

    Arreglado el archivo private y cambios en las propiedades de las variables(up to change)

--- tarea1_poo/src/tarea1_poo/Estudiante.java
+++ tarea1_poo/src/tarea1_poo/Estudiante.java
@@ -10,39 +10,15 @@ package tarea1_poo;
  */
 public class Estudiante {
 
-    private String nombre;
-    private String carrera;
-    private String id; // se usara el tipo de identificador de la universidad? xx-xxxx-xxxx? //
+String nombre;
+ String carrera;
+ String id; // se usara el tipo de identificador de la universidad? xx-xxxx-xxxx? //
     
   
     public Estudiante(String id, String nombre, String carrera) {
         this.id = id;
         this.nombre = nombre;
         this.carrera = carrera;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCarrera() {
-        return carrera;
-    }
-
-    public void setCarrera(String carrera) {
-        this.carrera = carrera;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
 }
