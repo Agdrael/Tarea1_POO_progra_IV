1a15ecce21a9d22d00afe295ffada1f8ebc7723c
cesar-045 <cesarflamenco202075@gmail.com>
2025-08-22 23:50:23 -0600

    Excepciones y Validaciones de variables.

--- tarea1_poo/src/tarea1_poo/Estudiante.java
+++ tarea1_poo/src/tarea1_poo/Estudiante.java
@@ -10,11 +10,33 @@ package tarea1_poo;
  */
 public class Estudiante {
 
-String nombre;
+ String nombre;
  String carrera;
  String id; // se usara el tipo de identificador de la universidad? xx-xxxx-xxxx? //
     
-  
+  /*
+  //Aquí estaría como se implemente la validación cuando de ingresa un dato
+        boolean validacion;
+        do {
+            System.out.println("Ingrese el nombre: ");
+            nombre=sc.nextLine();
+
+            if (validarNombre(nombre)){
+                validacion=false;
+            }else{
+                System.out.println("Dato ingresado incorrectamente. Intente nuevamente.");
+                validacion=true;
+            }
+        } while (validacion);
+
+  //Y este sería el Try & Catch de las variables numéricas
+        try{
+            System.out.println("Ingrese un número: ");
+            num=sc.nextInt();
+        }catch(Exception e){
+            System.out.println("Tipo de dato incorrecto: " + e);
+        }
+  */
     public Estudiante(String id, String nombre, String carrera) {
         this.id = id;
         this.nombre = nombre;
@@ -22,3 +44,52 @@ String nombre;
     }
 
 }
+/*
+-> MÉTODO PARA LAS VARIABLES COMO EL NOMBRE QUE SÓLO DEBE INCLUIR LETRAS <-
+
+    public static boolean validarLetras(String nombre){
+        
+        int contador=0;
+        for (int i = 0; i < nombre.length() ; i++) {
+            char caracter = nombre.charAt(i);
+            
+            if(!Character.isLetter(caracter)&& caracter !=' ' &&
+                caracter !='á' && caracter !='é' && caracter !='í' && 
+                caracter !='ó' && caracter !='ú' && caracter !='ñ' &&
+                caracter !='Á' && caracter !='É' && caracter !='Í' && 
+                caracter !='Ó' && caracter !='Ú' && caracter !='Ñ'){
+                contador++; //El contador aumenta si encuentra un caracter que no sea una letra.
+            }
+            
+        }
+        
+        if (contador == 0) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
+    -> MÉTODO PARA VARIABLES QUE SÓLO DEBEN INCLUIR NÚMEROS Y GUIONES <-
+        
+    public static boolean validarNumGuion(String nombre){
+        
+        int contador=0;
+
+        for (int i = 0; i < nombre.length() ; i++) {
+            char caracter = nombre.charAt(i);
+            
+            if(!Character.isDigit(caracter)&& caracter !='-'){
+                contador++;
+            }
+            
+        }
+        if (contador == 0) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+*/
